f83656835b27822e8d5ef97b7e658ebc908413e4
Anton-Kalyutich <anton.kalyutich@gmail.com>
2021-02-22 18:06:00 -0800

    Created comments table and Table model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_013746) do
+ActiveRecord::Schema.define(version: 2021_02_23_014239) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "post_id"
+    t.integer "author_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
